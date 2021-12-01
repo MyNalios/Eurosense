57ccbb29e549842a431b4a465b20cbc8698940f6
florvhee <florvhee@gmail.com>
2021-12-01 15:24:50 +0100

    add users relation to contact reverse

--- eurosense/models/user.py
+++ eurosense/models/user.py
@@ -4,4 +4,4 @@ class User(models.Model):
     _name = "res.users"
     _inherit = "res.users"
 
-    allowed_contact_ids = fields.Many2many(string="Allowed partners", comodel_name="res.partner", relation="allowed_user_partner", column1="user_id", column2="partner_id")
+    allowed_contact_ids = fields.Many2many(string="Allowed partners", comodel_name="res.partner", relation="allowed_user_partner", column1="user_id", column2="partner_id", copy=False)
