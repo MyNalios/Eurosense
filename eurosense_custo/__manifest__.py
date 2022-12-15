93b3a1331a54737a8d701295d360111890c8784c
Thomas Lelubre <tle@nalios.be>
2022-12-15 10:56:47 +0100

    [ADD]Création de la structure du module custo.

---
+++ eurosense_custo/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Eurosense Custo",
+
+    'summary': """
+        All Eurosense customization
+        """,
+
+    'description': """
+        All Eurosense customization
+    """,
+
+    'category': 'customization',
+
+    'depends': ['base'],
+
+    'data': [],
+
+    'license': 'LGPL-3',
+}
