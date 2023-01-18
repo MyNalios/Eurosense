9601db1eab4e7fea04061a6de8af9ba6ae418ac1
Pierre Locus <lop@nalios.be>
2023-01-18 16:27:00 +0100

    [FIX] Using hr.employee instead of res.users

--- eurosense_custo/models/hr_employee.py
+++ eurosense_custo/models/hr_employee.py
@@ -3,6 +3,6 @@ from odoo import api, fields, models
 class Hr_Employee(models.Model):
     _inherit = 'hr.employee'
 
-    allowed_timesheets_user_ids = fields.Many2many('hr.employee', string="Allowed timesheets users")
+    allowed_timesheets_user_ids = fields.Many2many('hr.employee', 'hr_employee_allowed_employee_rel', 'employee_one_id', 'employee_two_id', string="Allowed timesheets users")
 
     
