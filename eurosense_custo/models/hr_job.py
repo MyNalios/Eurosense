2ea7c4211cc122c65e247b0b9fedc0860e801a08
Pierre Locus <lop@nalios.be>
2023-07-11 10:34:47 +0200

    [FIX][2689] hr.applicant rules

--- eurosense_custo/models/hr_job.py
+++ eurosense_custo/models/hr_job.py
@@ -7,3 +7,8 @@ class HrJob(models.Model):
     _inherit = 'hr.job'
 
     allowed_recruiter_ids = fields.Many2many('res.users', 'hr_job_allowed_recruiter_user_rel')
+
+class HrApplicant(models.Model):
+    _inherit = 'hr.applicant'
+
+    allowed_recruiter_ids = fields.Many2many(related='job_id.allowed_recruiter_ids', store=True)
