fc25d7284a788a1189ae81971936b6d148a68c30
Pierre Locus <lop@nalios.be>
2023-07-11 10:41:23 +0200

    [FIX][2689] applicants

--- eurosense_custo/models/hr_job.py
+++ eurosense_custo/models/hr_job.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class HrJob(models.Model):
     _inherit = 'hr.job'
 
     allowed_recruiter_ids = fields.Many2many('res.users', 'hr_job_allowed_recruiter_user_rel')
+
+
+class HrApplicant(models.Model):
+    _inherit = 'hr.applicant'
+
+    allowed_recruiter_ids = fields.Many2many('res.users', compute='_compute_allowed_recruiter_ids', store=True)
+
+    @api.depends('job_id')
+    def _compute_allowed_recruiter_ids(self):
+        for rec in self:
+            if rec.job_id.allowed_recruiter_ids:
+                rec.allowed_recruiter_ids = [(6, 0, rec.job_id.allowed_recruiter_ids.ids)]
+            else:
+                rec.allowed_recruiter_ids = False
